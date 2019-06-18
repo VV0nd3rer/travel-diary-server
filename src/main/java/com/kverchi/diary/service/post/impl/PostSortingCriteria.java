a4f0ca73e6f77d55b936b70c484f88749e27f4ac
Liudmyla Melnychuk <kverchi24@gmail.com>
2019-06-18 13:34:33 +0200

    Added unsorted state for sorting criteria.

--- src/main/java/com/kverchi/diary/service/post/impl/PostSortingCriteria.java
+++ src/main/java/com/kverchi/diary/service/post/impl/PostSortingCriteria.java
@@ -4,6 +4,7 @@ package com.kverchi.diary.service.post.impl;
  * Created by Liudmyla Melnychuk on 24.5.2019.
  */
 public enum PostSortingCriteria {
+    UNSORTED,
     VISITED,
     WISHED,
     NEWEST,
